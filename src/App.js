bb1d90e8ec31b2a9d144d45ad9d8db7fb919bd39
Priyanka Raghuvanshi <enrich.samriddhi@gmail.com>
2025-04-20 20:50:38 +0530

    WORw

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import './App.css'; 
+import Header from './Header';
+import { useEffect} from 'react';
+
+function App() {
+return(
+  <div className="App">
+
+  <Header 
+  name ="Sam"
+  age={18}
+  />
+
+  
+
+
+  </div>
+
+);   
+}
+
+
+export default App ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
